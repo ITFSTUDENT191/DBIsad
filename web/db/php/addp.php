9f6215d4fc8e73a7966e617581de9ba8b00a3867
PeetTharathanakul <peet12131424@gmail.com>
2019-05-06 23:52:05 +0700

    .

--- web/db/php/addp.php
+++ web/db/php/addp.php
@@ -5,12 +5,12 @@ include 'connectDB.php';
 $p_code = $_POST['p_code'];
 $p_name = $_POST['p_name'];
 $p_price = $_POST['p_price'];
-$w_id = $_POST['w_id'];
+$w_id = $_POST['l_id'];
 $p_quantity = $_POST['p_quantity'];
 $cat_id = $_POST['cat_id'];
 $p_descript = $_POST['p_descript'];
 
-$sql = "INSERT INTO product(p_code,p_name,p_price,w_id,p_quantity,cat_id,p_descript) values ('$p_code','$p_name','$p_price','$w_id','$p_quantity','$cat_id','$p_descript')";
+$sql = "INSERT INTO product(p_code,p_name,p_price,l_id,p_quantity,cat_id,p_descript) values ('$p_code','$p_name','$p_price','$w_id','$p_quantity','$cat_id','$p_descript')";
 $result = $conn->query($sql);
 echo "Add product Success";
 
