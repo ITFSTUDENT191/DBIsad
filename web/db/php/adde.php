5be1186e758fae630a24f9037b8f05cb09f00bf7
natttakit <59070047@kmitl.ac.th>
2019-05-07 00:34:20 +0700

    inseert function to ramdompass to adde.php

--- web/db/php/adde.php
+++ web/db/php/adde.php
@@ -1,6 +1,9 @@
 <?php
 include 'connectDB.php';
-
+function conn(){
+		$conn = new mysqli("localhost", "root","", "stocking");
+		return $conn;
+	}
 
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -22,6 +25,9 @@ $result = $conn->query($sql);
 	} else {
 		echo "something error";
 	}
-
+$checkEmpID =  $conn->query("SELECT LAST(emp_id) FROM employees");
+$RanPass = rand(100000,999999);
+$sqlinsert = "INSERT INTO employees (emp_id,user_id,user_pass,emp_name,role) VALUES ('".$checkEmpID."', '".$_POST["user_id"]."', '".$ranpass."', '".$_POST["emp_name"]."', '".$_post["
+role"]."')""
 
 ?>
