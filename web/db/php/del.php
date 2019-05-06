4db9fdc9bd089ccff966a8566db0bf0d2d27ef6b
Pai <59070155@it.kmitl.ac.th>
2019-05-06 19:14:52 +0700

    Done

---
+++ web/db/php/del.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="add.php" method="POST">
+	<h1>Delete Emp</h1>
+	Employee Id: <input type="text" name="emp_id">
+	<br><br>
+	<input type="submit" value="ok">
+</body>
+</html>
