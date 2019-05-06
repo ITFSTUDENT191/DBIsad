17f8d94501de113534db2f16f7bd805ce2fd8688
PeetTharathanakul <peet12131424@gmail.com>
2019-05-06 21:42:11 +0700

    .

--- web/db/php/addEmp.php
+++ web/db/php/addEmp.php
@@ -1,13 +1,15 @@
-<?php  
-include 'connectDB.php';
-
-$user_id = $_POST['user_id'];
-$user_pass = $_POST['user_pass'];
-$emp_name = $_POST['emp_name'];
-$role = $_POST['role'];
-
-$sql = "INSERT INTO employees(user_id,user_pass,emp_name,role) values ('$user_id', '$user_pass', '$emp_name', '$role')";
-$result = $conn->query($sql);
-echo "Add Employee Success.";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+	</title>
+</head>
+<body>
+	<form action="adde.php" method="POST">
+	Username : <input type="text" name="user_id"><br>
+	Password : <input type="password" name="user_pass"><br>
+	Name : <input type="text" name="emp_name"><br>
+	Position :<input type="text" name="role"><br>
+	<input type="submit" value="submit">
+</body>
+</html>
