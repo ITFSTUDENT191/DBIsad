8067fdd73609380880d4b666e09b205124617618
PeetTharathanakul <peet12131424@gmail.com>
2019-05-06 21:17:39 +0700

    editemp

--- web/db/php/edit1.php
+++ web/db/php/edit1.php
@@ -4,10 +4,10 @@
 	<title></title>
 </head>
 <body>
-	<form action="edit2.php" method="POST">
-	<h1>Delete Emp</h1>
+	<form action="edite.php" method="POST">
+	<h1>Edit Employees</h1>
 	Employee Id: <input type="text" name="emp_id">
 	<br><br>
-	<input type="submit" value="ok">(ไปหน้าedit2 ดึงข้อมูลเก่าลงedit2)
+	<input type="submit" value="ok">
 </body>
 </html>
--- web/db/php/edit2.php
+++ web/db/php/edit2.php
@@ -4,11 +4,11 @@
 	<title></title>
 </head>
 <body>
-	<form action="__.php" method="POST">
+	<form action="editd.php" method="POST">
 	Username : <input type="text" name="user_id" value="___"><br>
 	Password : <input type="password" name="user_pass" value="___"><br>
 	Name : <input type="text" name="emp_name" value="___"><br>
 	Position :<input type="text" name="role" value="___"><br>
-	<input type="submit" value="submit">(เก็บเข้าdb)
+	<input type="submit" value="submit">
 </body>
 </html>
